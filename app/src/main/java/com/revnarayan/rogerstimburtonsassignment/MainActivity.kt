35f4c48daaee8e3009567e938302d57a56c3c1cf
Revathi Narayanan <reva1.narayan@gmail.com>
2021-05-18 01:30:49 +0530

    Step 12: Implement Navigation Drawer

--- app/src/main/java/com/revnarayan/rogerstimburtonsassignment/MainActivity.kt
+++ app/src/main/java/com/revnarayan/rogerstimburtonsassignment/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.revnarayan.rogerstimburtonsassignment
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.revnarayan.rogerstimburtonsassignment.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -13,5 +21,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.productsFragment
+            ), drawer_layout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupWithNavController(nav_view, navController)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // The action bar home/up action should open or close the drawer.
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
